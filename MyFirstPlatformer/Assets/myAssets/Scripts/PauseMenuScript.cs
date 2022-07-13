75d97c30a08e9ab583aafb9bde32283c7fe3c901
Arionixxx <vlad.leshchenko@gmail.com>
2022-07-14 00:44:12 +0300

    Add pause menu buttons, but it has bugs

--- MyFirstPlatformer/Assets/myAssets/Scripts/PauseMenuScript.cs
+++ MyFirstPlatformer/Assets/myAssets/Scripts/PauseMenuScript.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenuScript : MonoBehaviour
 {
     public GameObject pauseMenu;
     public bool isGamePaused = false;
 
+    public void continueButton()
+    {
+        pauseMenu.SetActive(false);
+        isGamePaused = false;
+        Time.timeScale = 1;
+    }
 
+    public void mainMenuButton()
+    {
+        SceneManager.LoadScene(0);
+    }
 
 
 
@@ -31,10 +42,7 @@ public class PauseMenuScript : MonoBehaviour
             }
             else
             {
-                pauseMenu.SetActive(false);
-                isGamePaused = false;
-                Time.timeScale = 1;
-                Debug.Log("not pause");
+                continueButton();
             }
         }
     }
